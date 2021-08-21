ae8687d4abef145338d5e35bcbb7c286bcdb0c07
Bianca da Silva Campos <51832213+BiancaSCampos@users.noreply.github.com>
2021-08-21 15:49:26 -0300

    Navigation bar created and stylization has begun

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+import { ContainerNavBar } from './styles';
+
+class NavBar extends Component {
+
+  render() {
+    return(
+      <ContainerNavBar>
+        <h1>Oi</h1>
+      </ContainerNavBar>
+    )
+  }
+}
+
+export default NavBar;
---
+++ src/components/NavBar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ContainerNavBar = styled.div`
+
+  background-color: #FBDBB5;
+  width: 100%;
+  height: 50%;
+`
