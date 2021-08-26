1b8d68f47b5c4ae478a99a262049c218a5e8f96a
Bianca da Silva Campos <51832213+BiancaSCampos@users.noreply.github.com>
2021-08-26 11:53:34 -0300

    Adding a GlobalStyles importation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import Routes from './routes';
+import GlobalStyles from './styles/GlobalStyles';
 
 
 function App() {
   return (
     <>
+    <GlobalStyles/>
     <Routes/>
     </>
   );
